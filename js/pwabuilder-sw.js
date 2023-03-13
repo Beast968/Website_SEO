033bf5bacd828d012432ca14f7c6a4abcc1e6d35
Beast968 <120974604+Beast968@users.noreply.github.com>
2023-03-13 16:04:25 +0300

    new file:   css/bootstrap.min.css 	new file:   css/style.css 	modified:   index.html 	new file:   js/bootstrap.min.js 	new file:   js/pwabuilder-sw.js 	new file:   robots.txt 	new file:   sitemap.xml 	new file:   sw.js

---
+++ js/pwabuilder-sw.js
@@ -0,0 +1,19 @@
+// This is the "Offline copy of pages" service worker
+// Website caching plugin
+
+const CACHE = "website_seo"; // The name of the file for the cache
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+workbox.routing.registerRoute(
+  new RegExp('/*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
+  })
+);
